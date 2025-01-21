8042edfd677fa407be2128b9950114d5010083f7
Felipe Rios <felipe.rios.silva@outlook.com>
2025-01-21 17:38:13 -0500

    chore(bump): bumped version to 2.0.2
    Signed-off-by: Felipe Rios <felipe.rios.silva@outlook.com>

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 // According to Terraform SDK documentation, the `main.go` should be on the root of the project.
 // Otherwise, `tfplugindocs generate` will not work and `.goreleaser.yml` should be changed.
 
-const version string = "2.0.1"
+const version string = "2.0.2"
 
 func main() {
 	var debug bool
